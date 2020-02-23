9e71145773286f2a3a550742567e948d8e175566
terranivium <wesleyscottuk@gmail.com>
2020-02-23 16:42:55 +0000

    merge tested with both string and int

--- AssEx1/AssessmentSLL.java
+++ AssEx1/AssessmentSLL.java
@@ -33,7 +33,6 @@ public class AssessmentSLL<E extends Comparable<E>> {
 			element = elem;
 			next = n;
 		}
-
 	}
 
 	// ////////////////////////////////
@@ -69,7 +68,6 @@ public class AssessmentSLL<E extends Comparable<E>> {
 			first = new Node<E>(elem, first);
 			return;
 		}
-		
 		newNode.next = null;
 
 		Node<E> last = first;
@@ -77,6 +75,7 @@ public class AssessmentSLL<E extends Comparable<E>> {
 			last = last.next;
 		}
 		last.next = newNode;
+
 		return;
 	}
 
@@ -96,12 +95,33 @@ public class AssessmentSLL<E extends Comparable<E>> {
 			// Change next link of previous node
 			pred.next = curr.next;
 
-			// Free node
+			// Clear current node
 			curr = null;
 
 			// Update pred and curr
 			pred = pred.next;
 			if (pred != null) curr = pred.next;
+		}
+	}
+
+	public Node<E> getNextUnique(Node<E> curr){
+		Node<E> dummy = curr;
+		curr = curr.next;
+		while(compare(dummy, curr) == 0){
+			curr = curr.next;
+		}
+		return curr;
+	}
+
+	public int compare(Node<E> elem1, Node<E> elem2){
+		if (elem1 == null && elem2 == null) { // null + equal
+			return 0;
+		} else if (elem1 == null) { // this null
+			return 1;
+		} else if (elem2 == null) { // arg null
+			return -1;
+		} else{ 
+			return elem1.element.compareTo(elem2.element); // zero if the strings are equal, positive if lex >, negative if lex <
 		}
 	}
 
@@ -112,28 +132,47 @@ public class AssessmentSLL<E extends Comparable<E>> {
 	 * with the new name of this class
 	 */
 	public  AssessmentSLL<E> merge(AssessmentSLL<E> list1, AssessmentSLL<E> list2){
-		AssessmentSLL<E> mergedList = new AssessmentSLL<E>();
-		// Merge lists
-
-
-
-
-
-		// Remove duplicates
-		Node<E> curr = first;
-
-		while(curr!=null){
-			Node<E> temp = curr;
-			while(temp!=null && temp == curr){
-				temp = temp.next;
-			}
-			curr.next = temp;
-			curr = curr.next;
-		}
 		
+		AssessmentSLL<E> mergedList = new AssessmentSLL<E>();
+		Node<E> a = this.first;
+		Node<E> b = list1.first;
+		Node<E> c = list2.first;
+
+		while(a != null || b != null || c != null){
+			if ((this.compare(a, b) <= -1) && (this.compare(a, c) <= -1)){ // b and c are larger than a or null
+				mergedList.insert(a.element);
+				a = getNextUnique(a);
+			} else if((this.compare(b, a) <= -1) && (this.compare(b, c) <= -1)){ // a and c are larger than b or null
+				mergedList.insert(b.element);
+				b = getNextUnique(b);
+			} else if((this.compare(c, a) <= -1) && (this.compare(c, b) <= -1)){ // a and b are larger than c or null 
+				mergedList.insert(c.element);
+				c = getNextUnique(c);
+			} else if ((this.compare(a, b) == 0) && (this.compare(a, c) <= -1)){
+				// ab equal, a < c
+				mergedList.insert(a.element);
+				a = getNextUnique(a);
+				b = getNextUnique(b);
+			} else if ((this.compare(a, c) == 0) && (this.compare(a, b) <= -1)){ 
+				// ac equal, a < b
+				mergedList.insert(a.element);
+				a = getNextUnique(a);
+				c = getNextUnique(c);
+			}  else if ((this.compare(b, c) == 0) && (this.compare(b, a) <= -1)){
+				// bc equal, b < a
+				mergedList.insert(b.element);
+				b = getNextUnique(b);
+				c = getNextUnique(c);
+			} else if ((this.compare(a, b) == 0) && (this.compare(b, c) == 0)){
+				mergedList.insert(b.element);
+				a = getNextUnique(a);
+				b = getNextUnique(b);
+				c = getNextUnique(c);
+			}
+		}
+		mergedList.printFirstToLast();
+
 		return mergedList;
-
 	}
-
 }
 
